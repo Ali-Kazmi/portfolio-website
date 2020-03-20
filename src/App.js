354e6f354ebefc6c9dfea8ef1f9a779923115105
ali <3ali.kazmi@gmail.com>
2020-03-20 00:20:32 -0700

    fixed dependencies, added initial scrollbar ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
  
 
   //rendering
   return (
+    <Router>
     <div className="App">
-      
+    <Nav />
     </div>
-  );
+    </Router>
+  )
 }
 
 export default App;
